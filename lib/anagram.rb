ccc8d15f3a7b9f246e8f32822137c8f965133537
James Kyan <james.kyan88@gmail.com>
2022-07-03 14:56:24 -0400

    commit tree 5d59aab2cbc48375479ef14c006c0ee44cff6a1b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        word_letters = @word.split('')
+        arr.filter do |word|
+            word.split('').sort == word_letters.sort
+        end
+    end
+
+
+end
+
+
+# binding.pry
